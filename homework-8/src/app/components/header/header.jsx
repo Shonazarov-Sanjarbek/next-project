83b9ced30e8d9ebacd9c5e4a1b47d1c5998f1c78
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-10-14 09:22:45 +0500

    S

---
+++ homework-8/src/app/components/header/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div className='text-center'>
+        assalomu alekum ustoz vazifani nextda qila olmadim shunga https://github.com/Shonazarov-Sanjarbek/project shu link orqali tekshrshingizni sorab qolaman
+    </div>
+  )
+}
